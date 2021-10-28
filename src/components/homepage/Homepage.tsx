b2772ad51a4516fa358b3445d3cae648856803a3
Thomhas <thomas.hasvold@gmail.com>
2021-10-28 15:11:44 +0200

    first commit

---
+++ src/components/homepage/Homepage.tsx
@@ -0,0 +1,17 @@
+import "./Homepage.css";
+
+const Homepage = () => {
+  return (
+    <div id="HomepageWrapper">
+      <h1>
+        Hi,
+        <br /> I'm <span>T</span>homas, <br />
+        software developer
+      </h1>
+      <p>Front End - / Back End Developer</p>
+      <button>Contact me! </button>
+    </div>
+  );
+};
+
+export default Homepage;
